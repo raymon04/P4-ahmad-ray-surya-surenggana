6140e12b433ba837a149a51672d9d06912a44705
raymon04 <64573865+raymon04@users.noreply.github.com>
2020-05-11 20:09:18 +0700

    Create jobsheet  10 C++.cpp
     Buatlah Program matriks tiga dimensi dengan menggunakan Nama orang mahasiswa

---
+++ jobsheet  10 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main (){
+		cout<<"Nama : Ahmad Ray Surya Surenggana\n";
+	cout<<"Nim : F1B019013\n";
+	cout<<"Kelompok: Kelompok 4\n\n";
+char nama [2][2][10];
+int i, j;
+for (i = 0; i< 2; i++){
+for (j = 1; j < 10; j++){
+cout<< "nama my best friend  [" <<i<<"][" << j << "] = ";
+cin>>nama[i][j];
+}
+}
+}
