2fb8e461aa774c17604ac50aebf08d823beef3bd
raymon04 <64573865+raymon04@users.noreply.github.com>
2020-05-11 19:56:35 +0700

    Create jobsheet 7 C++.cpp
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM terakhir setiap Mahasiswa

---
+++ jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ; 
+
+int main (){
+	cout<<"Nama : Ahmad Ray Surya Surenggana\n";
+	cout<<"Nim : F1B019013\n";
+	cout<<"Kelompok: Kelompok 4\n\n";
+	int i ;
+	int matriks[13] ;
+	 for (i=1 ; i<=13 ;i++){
+	 	cout << "Nilai index ke-" << i<< " = " ;
+	 	cin >> matriks[i] ;
+	 }
+	 cout <<endl ; 
+}
