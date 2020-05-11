acac18136d518fd23b063ef656474519e2889af4
raymon04 <64573865+raymon04@users.noreply.github.com>
2020-05-11 16:52:53 +0700

    Create jobsheet 1 C.c
    Buatlah program array satu dimensi yang menampilkan NIM masing masing

---
+++ jobsheet 1 C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main (){
+int nim[13]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+printf ("nama : ahmad ray surya surenggana\n");
+printf ("nim : F1B019013\n ");
+printf ("kelompok : 4\n");
+printf ("Data ke-1 = %i\n",nim[0]);
+printf ("Data ke-2 = %i\n",nim[1]);
+printf ("Data ke-3 = %i\n",nim[2]);
+printf ("Data ke-4 = %i\n",nim[3]);
+printf ("Data ke-5 = %i\n",nim[4]);
+printf ("Data ke-6 = %i\n",nim[5]);
+printf ("Data ke-7 = %i\n",nim[6]);
+printf ("Data ke-8 = %i\n",nim[7]);
+printf ("Data ke-9 = %i\n",nim[8]);
+printf ("Data ke-10 = %i\n",nim[9]);
+printf ("Data ke-11 = %i\n",nim[10]);
+printf ("Data ke-12 = %i\n",nim[11]);
+printf ("Data ke-13 = %i\n",nim[12]);
+}
